e272a002244dedd07eb75b5d55a3b451c1855600
muztishini <111636144+muztishini@users.noreply.github.com>
2024-11-22 11:02:10 +0500

    add json file

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import json
+
+
 class Book:
     def __init__(self, id: int, title: str, author: str, year: int, status: str) -> None:
         self.id = id
@@ -6,8 +9,38 @@ class Book:
         self.year = year
         self.status = status
 
-    def __str__(self) -> dict:
-        return {"id": self.id, "title": self.title, "author": self.author, "year": self.year, "status": self.status}
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "author": self.author,
+            "year": self.year,
+            "status": self.status
+        }
+
+
+# функция добавления книги
+def add_book() -> None:
+    id: int = int(input("Введите id: "))
+    title: str = input("Введите название: ")
+    author: str = input("Введите автора: ")
+    year: int = int(input("Введите год издания: "))
+    status: str = input("Введите статус: ")
+    book: Book = Book(id, title, author, year, status)
+    # Если файл не существует, создаем пустой список книг
+    try:
+        with open('library.json', 'r', encoding='utf-8') as file:
+            books = json.load(file)
+    except json.JSONDecodeError:
+        books = []
+
+    # Добавляем новую книгу в список
+    books.append(book.to_dict())
+    
+    # Сохраняем список в файл
+    with open('library.json', 'w', encoding='utf-8') as file:
+        json.dump(books, file, ensure_ascii=False, indent=4)
+        print("Книга добавлена")
 
 
 # главная функция
@@ -25,7 +58,7 @@ def main():
             case "1":
                 pass
             case "2":
-                pass
+                add_book()
             case "3":
                 pass
             case "4":
